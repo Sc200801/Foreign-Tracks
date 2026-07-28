d8a91eb39a0855f4bcd95cf6aedd29da723b9656
Fabiola Ardón <fabiolaardon2008@gmail.com>
2026-07-28 11:50:10 -0600

    feat:creando nueva rama

--- src-server/config/db.js
+++ src-server/config/db.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log('--- VERIFICANDO VARIABLES QUE LEE NODE ---');
+console.log('HOST:', process.env.DB_HOST);
+console.log('USER:', process.env.DB_USER);
+console.log('DB NAME:', process.env.DB_NAME);
+console.log('PORT:', process.env.DB_PORT);
+console.log('-----------------------------------------');
+
 // Inicialización de Sequelize con MariaDB
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -10,7 +17,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mariadb',
-    logging: false, // Cambia a console.log si quieres ver las consultas SQL en la terminal
+    logging: console.log, // Cambia a console.log si quieres ver las consultas SQL en la terminal
   }
 );
 
